e03c4f0e3d7cfcad14bcdfb2925ad60bdb1ddf6b
melihsahtulek <melihsahtulek@gmail.com>
2022-01-23 01:38:06 +0300

    wip

--- regex-basic/index.js
+++ regex-basic/index.js
@@ -1,6 +1,59 @@
 console.clear();
 
-let re1 = new RegExp("abc");
-let re2 = /abc/;
+/abc/.test("abcde"); // true
+/abc/.test("abxde"); // false
 
+/[0123456789]/.test("in 1992"); // true
+/[0-9]/.test("in 1992"); // true
+
+/'\d+'/.test("'123'"); // true
+/'\d+'/.test("'123ABC'"); // false
+/'\d+'/.test("''"); // false
+
+/sayHe?llo/.test("sayHello"); // true
+/sayHe?llo/.test("sayHllo"); // true
+
+console.log(/\d{1,2}-\d{1,2}-\d{4}/);
+
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
 // https://eloquentjavascript.net/09_regexp.html#p_0mNIcPpslS
