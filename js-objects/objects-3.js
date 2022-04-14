8a30332fd601aa7b5795dfbc60ecd13abaed2329
melihsah <melihsahtulek@gmail.com>
2022-04-14 19:25:09 +0300

    objects ok

--- js-objects/objects-3.js
+++ js-objects/objects-3.js
@@ -30,6 +30,7 @@ const personObj_3 = {
   age: 50,
   job: "Manager",
   fullName() {
+    // f: function() {}
     return this.name + " " + this.surname;
   },
   address: {
@@ -41,7 +42,88 @@ console.log(personObj_3.fullName());
 console.log("====================================");
 
 console.log("====================================");
-let userAge = personObj_3.age;
-let { age } = personObj_3;
-console.log(userAge, age);
+let { name: userName, age, job: userJob } = personObj_3;
+console.log(userName, age, userJob);
+console.log("====================================");
+
+console.log("====================================");
+const persons = [
+  {
+    name: "Jhon",
+    surname: "Doe",
+    age: 50,
+  },
+  {
+    name: "Jane",
+    surname: "Doe",
+    age: 40,
+  },
+];
+
+for (const { name } of persons) {
+  console.log(name);
+}
+console.log("====================================");
+
+console.log("====================================");
+const personObj_4 = {
+  name: "Jhon",
+  surname: "Doe",
+  age: 50,
+  job: "Manager",
+  fullName() {
+    return this.name + " " + this.surname;
+  },
+  address: {
+    city: "Antalya",
+    district: "Alanya",
+  },
+};
+
+const personObj_5 = { ...personObj_4 };
+const schoolObj = {
+  school: "abc University",
+};
+
+// console.log(Object.assign(personObj_5, schoolObj));
+console.log({ ...personObj_5, ...schoolObj });
+console.log("====================================");
+
+console.log("====================================");
+const personObj_6 = {
+  name: "Jhon",
+  surname: "Doe",
+  age: 50,
+  job: "Manager",
+  fullName() {
+    return this.name + " " + this.surname;
+  },
+  address: {
+    city: "Antalya",
+    district: "Alanya",
+  },
+};
+
+let { address: userAddress, ...rest } = personObj_6;
+console.log(userAddress, rest);
+console.log("====================================");
+
+console.log("====================================");
+const personObj_7 = {
+  name: "Jhon",
+  surname: "Doe",
+  age: 50,
+  job: "Manager",
+  fullName() {
+    return this.name + " " + this.surname;
+  },
+  address: {
+    city: "Antalya",
+    district: "Alanya",
+  },
+};
+
+console.log(Object.keys(personObj_7));
+console.log(Object.values(personObj_7));
+console.log(Object.entries(personObj_7));
 console.log("====================================");
